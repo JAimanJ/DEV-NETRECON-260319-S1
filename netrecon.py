cc6575b4a87cfdf4b2aa975e9fd8abd4b49dbea0
ILikeWaterDry <152250458+JAimanJ@users.noreply.github.com>
2026-03-19 14:53:20 +0800

    Port scanning test

--- netrecon.py
+++ netrecon.py
@@ -3,3 +3,29 @@ import subprocess
 import sys
 from datetime import datetime
 from concurrent.futures import ThreadPoolExecutor, as_completed
+
+# common ports as searched online
+
+commonPorts = [
+    21, 22, 23, 25, 53, 80, 110, 135, 139, 143, 443, 445, 3306, 8080 
+]
+
+def portScan(ip, port):
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(0.5)
+        result = sock.connect_ex((ip, port))
+        sock.close()
+
+        if result == 0:
+            return True
+        return False
+    except Exception:
+        return False
+    
+
+#Testing(uncomment to test)
+#ip = "scanme.nmap.org"  #a legal safe test plaec
+#for port in commonPorts:
+#    if portScan(ip, port):
+#        print(f"[Open] {ip}:{port}")
